479ebd4c1917c2e21362498f0294a8042cd63be8
Louis Bertrand <adressepro111@pylott.yt>
2020-03-19 02:42:55 +0100

    000001
    File system
    Invoke method fixe
    Config system
    custom in-cmd throwing handeler

--- src/studio/louisbertrand/autorestart/Main.java
+++ src/studio/louisbertrand/autorestart/Main.java
@@ -1,16 +1,7 @@
 package studio.louisbertrand.autorestart;
 
-import java.io.File;
-import java.io.IOException;
-//import java.lang.reflect.Array;
-import java.lang.reflect.InvocationTargetException;
-
-/* Class Importations
- * 
- * imported:
- * 	org.bukkit.command.Command
- *  org.bukkit.command.CommandSender
- *  org.bukkit.plugin.java.JavaPlugin
+/**
+ *  Class Importations
  */
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -18,6 +9,14 @@ import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.io.File;
+import java.io.IOException;
+// import java.lang.reflect.Array;
+// import java.lang.reflect.InvocationTargetException;
+import java.nio.charset.StandardCharsets;
+
+// import javax.annotation.Nonnull;
+import com.google.common.io.Files;
 
 /**
  * Main class
@@ -31,18 +30,47 @@ public class Main extends JavaPlugin {
 
     private final File[] customConfigFiles = new File[99];
     public FileConfiguration[] customConfigs = new YamlConfiguration[99];
+
+    private final File[] customLangsFiles = new File[99];
+    public String[] customLangs = new String[99];
+    
     public boolean valid = true;
     public boolean error = false;
+    public boolean warning = this.valid;
 
+    // YML CONFIG FILES IDS SHORTCUTS
     public Integer ConfigCommandsID = 0;
+
+    // TXT LANGS FILES IDS SHORTCUTS
+    public Integer LangHelpStringID = 0;
     
     public defaultCommandParser commandP;
     
     @Override
     public void onEnable() {
-        this.createCustomConfig("commands",this.ConfigCommandsID);
-        this.commandP = new defaultCommandParser(this, "autorestart", ConfigCommandsID);
-    	System.out.println("[AUTORESTART] Activated");
+    	
+    	try {
+	    	// Loading Command Config
+	        this.createCustomConfig("commands", this.ConfigCommandsID);
+	        
+	        // Loading Lang File for Help in english
+	        this.loadLangFileByName("lang/help.en.txt", this.LangHelpStringID);
+	        
+	        // declaring default command
+	        this.commandP = new defaultCommandParser(this, "autorestart", ConfigCommandsID);
+	        
+	        
+	        /**
+	         * IF RETURNED,
+	         * IT MEENS THAT EVERYTHING IS WELL IMPORTED/ACTIVATED/DECLARED
+	         */
+	    	System.out.println("[AUTORESTART] Activated");
+    	}catch(Throwable e){
+    		
+    		e.printStackTrace();
+    		throw e;
+    		
+    	}
     }
 
     @Override
@@ -73,24 +101,37 @@ public class Main extends JavaPlugin {
         if (command.getName().equalsIgnoreCase("autorestart")) {
             commandResponse parsed;
 
+            /*
+            // List Arguments
+            // DEBUG PURPOSE ONLY
             for (int i = 0 ; i < args.length ; i++) {
                 sender.sendMessage("args["+i+"]");
                 sender.sendMessage(args[i]);
-            }            
+            } 
+            */           
             
 			try {
+	            // Parsing Command
 				parsed = this.commandP.parse(args, sender);
+				
+				// Test if the response is not a valid response
 	            if(parsed.code != 0){
 	            	
+		            // Send response
 	                sender.sendMessage(parsed.message);
 	                if(parsed.code == 2) {
-	    	            return this.valid;
+	    	            // Return if Warning
+	    	            return this.warning;
 	                }
+    	            // Return error
 	                return this.error;
 	            }
+	            // Return valid
 	            return this.valid;
+            	
 			} catch (final Throwable e) {
 				
+				// in case of fatal error, throw the error
 				e.printStackTrace();
 		        return this.error;
 			}
@@ -128,5 +169,36 @@ public class Main extends JavaPlugin {
             e.printStackTrace();
         }
     }
+
+    /**
+     * createCustomConfig
+     * 
+     * Loads custom configuration File with defined by a name and stores
+     * it inside a public variable with a defined Identifier
+     * 
+     * @param $name
+     * the name of the file
+     * 
+     * @param id
+     * the id assigned to the configuration file
+     * 
+     * @return void
+     */
+    public void loadLangFileByName(final String $name, final Integer id) {
+        try {
+        	File tmpfile;
+        	tmpfile = new File(this.getDataFolder().toString()+"/"+$name);
+        	if(!tmpfile.exists()) {
+        		tmpfile.getParentFile().mkdirs();
+                this.saveResource($name, false);
+        	}
+
+            this.customLangsFiles[id] = tmpfile;
+            this.customLangs[id] = Files.toString(tmpfile, StandardCharsets.UTF_8);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+    
     
 }
--- src/studio/louisbertrand/autorestart/defaultCommandParser.java
+++ src/studio/louisbertrand/autorestart/defaultCommandParser.java
@@ -3,9 +3,15 @@ package studio.louisbertrand.autorestart;
 //import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+
+// import com.google.common.io.Files;
+
+// import java.io.File;
+// import java.io.IOException;
 import java.lang.reflect.Array;
-import java.lang.reflect.InvocationTargetException;
+// import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+// import java.nio.charset.StandardCharsets;
 
 import javax.annotation.Nonnull;
 import org.bukkit.ChatColor;
@@ -111,12 +117,22 @@ public class defaultCommandParser{
      */
     public commandResponse commandParsingArgumentHelp(final Main plugin, final String[] args, final CommandSender sender) {
 
-        sender.sendMessage("Hellow");
-
-        final commandResponse response = new commandResponse();
-        response.message = "world";
-        response.code = 0;
-        return response;
+    	String message;
+    	
+    	try {
+			message = plugin.customLangs[plugin.LangHelpStringID];
+	        sender.sendMessage(logParser(message,0));
+	        final commandResponse response = new commandResponse();
+	        response.code = 0;
+	        response.message = "help has been shown";
+	        return response;
+		} catch (Throwable e) {
+	        final commandResponse response = new commandResponse();
+	        response.code = 1;
+	        response.message = logParser("ERROR::"+e.getMessage(), response.code);
+            e.printStackTrace();
+	        return response;
+		}
     }
 
     /**
@@ -134,34 +150,32 @@ public class defaultCommandParser{
     public commandResponse parse(final String[] args, final CommandSender sender) throws Throwable {
 
     	try {
+    		
+    		/**
+    		 * If no arguments passed,
+    		 * return the help function
+    		 */
         	if (args.length == 0) {
         		
+        		// Invoke Setup
                 final Class<?>[] argumentsTypes = {this.plugin.getClass(), args.getClass(), CommandSender.class};
                 final Object[] arguments = {this.plugin, args, (CommandSender) sender};
                 
+                // retrieving the function name associated to the first cmd argument
                 String methodName = this.dictionary.getString("help.function");
                 
-                // sender.sendMessage(methodName);
-                
+                // Actual invoking
             	final Method method = defaultCommandParser.class.getDeclaredMethod(methodName , argumentsTypes);
-            	final commandResponse response = (commandResponse) method.invoke(null, arguments);
+            													 // NOTE:   use 'this' to say who is triggering (Grooble)
+            	final commandResponse response = (commandResponse) method.invoke(this, arguments);
             	return response;
-        		/*
-                try {
-                    Class<?>[] argumentsTypes = {this.plugin.getClass(), args.getClass(), sender.getClass()};
-                    Object[] arguments = {this.plugin, args, sender};
-                	Method method = this.getClass().getDeclaredMethod(this.dictionary.getString("help.function"), argumentsTypes);
-                	commandResponse response = (commandResponse) method.invoke(null, arguments);
-                	return response;
-                } catch (Throwable e){
-                    final commandResponse response = new commandResponse();
-                    response.code = 1;
-                    response.message = logParser("INTERNAL SERVER ERROR : "+e.toString(), response.code);
-                	System.out.println(e.toString());
-                    return response;
-                }*/
+            	
         	}
-        	
+
+    		/**
+    		 *  If the argument passed is not defined in commands.yml,
+    		 * return an error to indicate the miss match
+    		 */
             if(!this.dictionary.getKeys(false).contains(args[0])){
                 final commandResponse response = new commandResponse();
                 response.code = 1;
@@ -171,21 +185,30 @@ public class defaultCommandParser{
                 return response;
             }
 
+            /**
+             * else
+             * return the response of the desired function according to the
+             * function associated to the first argument
+             */
+    		// Invoke Setup
             final Class<?>[] argumentsTypes = {this.plugin.getClass(), args.getClass(), CommandSender.class};
             final Object[] arguments = {this.plugin, args, (CommandSender) sender};
             
-            // final String methodName = this.dictionary.getString(args[0]+".function");
+            // retrieving the function name associated to the first cmd argument
+            String methodName = this.dictionary.getString(args[0]+".function");
 
-        	final Method method = defaultCommandParser.class.getDeclaredMethod(this.dictionary.getString(args[0]+".function") , argumentsTypes);
-        	final commandResponse response = (commandResponse) method.invoke(null, arguments);
+        	final Method method = defaultCommandParser.class.getDeclaredMethod(methodName , argumentsTypes);
+															 // NOTE:   use 'this' to say who is triggering (Grooble)
+        	final commandResponse response = (commandResponse) method.invoke(this, arguments);
         	return response;
     		
     	} catch (final Throwable e) {
 
             final commandResponse response = new commandResponse();
             response.code = 1;
-            response.message = logParser("ERROR::"+e.toString()+"->"+e.getLocalizedMessage()+"\n"+"Might be caused by a unavailability of the config file", response.code);
-    		
+            response.message = logParser("ERROR::"+e.toString()+"->"+e.hashCode()+"\n"+"Might be caused by a unavailability of the config file", response.code);
+
+            e.printStackTrace();
     		return response;
     		//throw e;
     		
